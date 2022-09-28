ca85ec8a09ab32884217cbae15eb8582a3171a94
yebrai <ivansevv@gmail.com>
2022-09-28 17:44:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+console.log("test");
+
+const canvas = document.querySelector("#my-canvas");
+const ctx = canvas.getContext("2d");
+canvas.style.backgroundColor = "black";
+
+// variables globales
+let ballX = 50;
+let ballY = 50;
+let ballDirectionX = 1;
+// la pelotita va hacia la derecha: 1
+// la pelotita va hacia la izquierda: -1
+let ballDirectionY = 1;
+let paletaX = 150;
+let paletaY = 560;
+let canvasWidth = canvas.width - 10
+let paletaHeight = -10
+let speed = 2
+let randomColor = "white"
+//"Math.floor(Math.random()*16777215).toString(16)"
+let isGameOn = true // el juego sigue andando
+
+// funciones del juego
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = "white";
+  ctx.arc(ballX, ballY, 10, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+};
+
+const drawPaleta = () => {
+  ctx.fillStyle = "grey";
+  ctx.fillRect(paletaX, paletaY, 150, 20)
+};
+
+const movePelotita = () => {
+  ballX = ballX + ballDirectionX * 2;
+  ballY = ballY + ballDirectionY * 2;
+};
+
+const pelotitaPaletaCollision = () => {
+
+    // ballX
+    // paletaX 
+    
+    // ballY
+    // paletaY
+    if (ballY > paletaY + paletaHeight && ballX > paletaX && ballX < (paletaX + 150)) {
+        // la pelota ha pasado el punto de la paleta
+        console.log("botes")
+        ballDirectionY = -1
+        ctx.fillStyle = randomColor
+        
+        
+    }
+
+    // estan colisionando
+}
+
+const pelotitaWallCollision = () => {
+  if (ballX > canvasWidth) {
+    //colision con pared derecha
+    //cambiar la direccion de la pelotita
+    ballDirectionX = -1;
+  }
+
+  if (ballY > canvas.height) {
+    isGameOn = false;
+
+  }
+
+  if (ballX < 0) {
+    ballDirectionX = 1;
+  }
+
+  if (ballY < 0) {
+    ballDirectionY = 1;
+  }
+};
+
+const gameLoop = () => {
+  //console.log("Ejecutando la recursion del juego")
+
+  // 1. Limpiar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // 2. Acciones y movimientos de los elementos del juego
+  movePelotita();
+  pelotitaWallCollision();
+  pelotitaPaletaCollision();
+
+  // 3. dibujado de los elementos
+  drawBall();
+  drawPaleta()
+
+  // controlar la recuersion del juego
+  if (isGameOn === true ) {
+
+      requestAnimationFrame(gameLoop);
+    }
+};
+
+window.addEventListener("keydown", (event) => {
+ //   console.log(event.code)
+    if (event.code === "ArrowRight" || event.code === "KeyD") {
+        paletaX = paletaX + 20
+    }
+    if (event.code === "ArrowLeft" || event.code === "KeyA") {
+        paletaX = paletaX - 20
+    }
+})
+
+gameLoop();
+
+
+//bonus limpiar colisiones.
+// incluir score
+// aumentar velocidad
+//boton para reiniciar (Window.reload)
+// mejorar forma de paleta y que esta no salga de los bordes "Siempre se mueve a la derecha o izquierda"
+//reducir paleta cuanto mas rebotes
